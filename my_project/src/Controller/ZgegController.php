a7642bca85ad32401244a36c80d1a9c98c9b283f
LeRomainMalin <romain.remaud@viacesi.fr>
2018-10-09 16:29:50 +0200

    Le message de Romain qui est développeur comme jaja.

---
+++ my_project/src/Controller/ZgegController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ZgegController extends AbstractController
+{
+    /**
+     * @Route("/query", name="query")
+     */
+    public function query()
+    {
+		$apiKey = '7712a22';
+		$query = "American";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://www.omdbapi.com/?s=' . $query . '&apiKey=' . $apiKey);
+        curl_setopt($ch,  CURLOPT_RETURNTRANSFER, true);
+
+        $resultat = curl_exec($ch);
+		
+		$json =json_decode ($resultat);
+		
+        return $this->render('zgeg/index.html.twig',
+			array(
+				'query' => 'valeur1',
+				'movies' => $json->Search
+			));
+    }
+	
+	  /**
+     * @Route("/film/{query}", name="film")
+     */
+    public function film( $query )
+    {
+		$apiKey = '7712a22';
+		//$query = "American";
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://www.omdbapi.com/?s=' . $query . '&apiKey=' . $apiKey);
+        curl_setopt($ch,  CURLOPT_RETURNTRANSFER, true);
+
+        $resultat = curl_exec($ch);
+		
+		$json =json_decode ($resultat);
+		
+        return $this->render('zgeg/index.html.twig',
+			array(
+				'query' => 'valeur1',
+				'movies' => $json->Search
+			));
+    }
+}
